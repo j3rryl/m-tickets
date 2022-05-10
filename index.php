210d2f2719cd82173dbdd120b6ee736cb41e5bf0
j3rryl <jeremymunroe0@gmail.com>
2022-05-10 18:51:01 +0300

    Initial Commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/index.css">
+    <title>M-Ticket</title>
+</head>
+<body>
+    <?php
+    require_once 'views/includes/header.php'
+    ?>
+    <h3>MTickets</h3>
+</body>
+</html>
---
+++ views/includes/header.php
@@ -0,0 +1,12 @@
+
+<header>
+    <nav>
+        <div class="logo">M-Tickets</div>
+        <ul>
+            <li><a class="active">Home</a></li>
+            <li><a>Book</a></li>
+            <li><a>About</a></li>
+        </ul>
+        <button>Contact</button>
+    </nav>
+</header>
