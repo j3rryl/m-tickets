0c6cdd34b08eee3b13557caf3d91bded933d7645
j3rryl <jeremymunroe0@gmail.com>
2022-06-23 19:40:25 +0300

    Footer links

--- views/templates/footer.php
+++ views/templates/footer.php
@@ -20,17 +20,19 @@
 
         <div class="box">
             <h3>contact info</h3>
-            <a href="#"> <i class="fas fa-phone"></i> +254 790 229 229 </a>
-            <a href="#"> <i class="fas fa-envelope"></i> info@mtickets.com </a>
-            <a href="#"> <i class="fas fa-map"></i> Nairobi, Kenya - 400104 </a>
+            <a href="tel:+254790229229"> <i class="fas fa-phone"></i> +254 790 229 229 </a>
+            <a href="mailto: info@mtickets.com"> <i class="fas fa-envelope"></i> info@mtickets.com </a>
+            <a target="_blank" href="https://www.google.com/maps/place/Kenya+Rd,+Nairobi/@-1.300702,36.813067,17z/data=!3m1!4b1!4m5!3m4!1s0x182f10e587b76c03:0x2227e7095cd95fcf!8m2!3d-1.3007074!4d36.8152557"> <i class="fas fa-map"></i> Nairobi, Kenya - 400104 </a>
         </div>
 
         <div class="box">
             <h3>follow us</h3>
-            <a href="#"> <i class="fab fa-facebook-f"></i> facebook </a>
-            <a href="#"> <i class="fab fa-twitter"></i> twitter </a>
-            <a href="#"> <i class="fab fa-instagram"></i> instagram </a>
-            <a href="#"> <i class="fab fa-linkedin"></i> linkedin </a>
+            <a target="_blank" href="https://www.facebook.com/mticketske/"> <i style="color: #4267B2;" class="fab fa-facebook-f"></i> facebook </a>
+            <a target="_blank" href="https://twitter.com/mtickets_news?lang=en"> <i style="color: #00acee;" class="fab fa-twitter"></i> twitter </a>
+            <a target="_blank" href="https://www.instagram.com/mticketsevents_/?hl=en"> <i 
+            style="color:lightgray;" 
+            class="fab fa-instagram"></i> instagram </a>
+            <a target="_blank" href="https://www.youtube.com/channel/UCxVsh1r80NDaG9kPuuF2fUA"> <i style="color: red;" class="fab fa-youtube"></i> youtube </a>
         </div>
 
     </div>
